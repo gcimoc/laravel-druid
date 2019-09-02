0e2d123c609b6c7c8c7b2c2508e46aba91c934d2
idominguez <israel.dominguez.garcia@gmail.com>
2019-09-02 17:01:46 +0200

    laravel druid logout

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -3,11 +3,11 @@
 Route::group(['middleware' => ['web']], function () {
     Route::prefix('actions')->group(function () {
         Route::group(['namespace' => 'Genetsis\Druid\Controllers'], function () {
-            Route::get('logout', 'ActionsController@logout')->name('actions.logout');
             Route::get('login', 'ActionsController@login');
             Route::get('register', 'ActionsController@register');
         });
 
+        Route::get('logout', config('druid.callback_controller').'@logout')->name('actions.logout');
         Route::get('callback', config('druid.callback_controller').'@index');
     });
 });
