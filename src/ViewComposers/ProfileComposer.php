969ca94644641cc284960822dc38dd126c7b9cf5
idominguez <israel.dominguez.garcia@gmail.com>
2019-10-16 10:35:22 +0200

    pass entry point or default

--- src/ViewComposers/ProfileComposer.php
+++ src/ViewComposers/ProfileComposer.php
@@ -4,6 +4,7 @@ use Genetsis\Identity;
 use Genetsis\URLBuilder;
 use Genetsis\Urls\DruidUrl;
 use Genetsis\UserApi;
+use Illuminate\Support\Arr;
 use Illuminate\View\View;
 
 class ProfileComposer
@@ -34,13 +35,16 @@ class ProfileComposer
         \App::make('Druid');
 
         $view->with('is_connected', Identity::isConnected());
-        $view->with('entry_point', Identity::getOAuthConfig()->getDefaultSection());
+
+        $entry_point = Arr::get($view->getData(), 'entry_point', Identity::getOAuthConfig()->getDefaultSection());
+        $view->with('entry_point', $entry_point);
+
         if (Identity::isConnected()) {
             $view->with('oid', UserApi::getUserLoggedOid());
             $view->with('edit_account_url',  URLBuilder::create(DruidUrl::EDIT)->get());
             $view->with('logout_url', route('actions.logout'));
         } else {
-            $view->with('login_url', URLBuilder::create(DruidUrl::LOGIN)->setScope(Identity::getOAuthConfig()->getDefaultSection())->get());
+            $view->with('login_url', URLBuilder::create(DruidUrl::LOGIN)->setScope($entry_point)->get());
         }
     }
 }
